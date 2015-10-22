c1df30200697e967eb682296fc1560b763a1bd44
Dennis Haegler <dennis.haegler@gmail.com>
2015-10-22 22:09:42 +0200

    Add book and author models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022124231) do
+ActiveRecord::Schema.define(version: 20151022192758) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.date     "published_at"
+    t.string   "description"
+    t.string   "language"
+    t.string   "image_link"
+    t.integer  "pages"
+    t.string   "isbn"
+    t.string   "isbn_13"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
