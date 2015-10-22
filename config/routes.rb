6fdd69c6b393050d1ce2344ab13b6fbf82024837
Dennis Haegler <dennis.haegler@gmail.com>
2015-10-22 15:03:05 +0200

    Add Twitter login.
    Twitter registration is added. App sent token to
    twitter, authorise and callback to our
    application. 
    
    The callback from twitter will be not handled by 
    this commit!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  devise_for :users
+  devise_for :users,
+    :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "home#index"
 end
