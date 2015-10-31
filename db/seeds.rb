f863bdaf443f49fd9ba47d358f59ccd735b643cc
Dennis Haegler <dennis.haegler@gmail.com>
2015-10-31 14:28:16 +0100

    Add simple seeding for dummy data
    This is used for development and will be replaced
    later on with real data to init the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..100).each do |index|
+  Book.create(title: "Book_#{index}",
+              isbn: "123345689#{index}",
+              isbn_13: "123455#{index}",
+              pages: index)
+end
+
+Author.create(name: "Test One")
+Author.create(name: "Two Test")
+Author.create(name: "Mr Test Three")
