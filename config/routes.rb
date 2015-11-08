b2efebe5d7fcc8e610e7e4b6f034230333771c34
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-08 13:52:47 +0100

    Add a route infrastructure for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :book, only: [:index, :show]
+  resources :books, only: [:index, :show]
   post '/books', to: 'books#search', as: 'book_search'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
