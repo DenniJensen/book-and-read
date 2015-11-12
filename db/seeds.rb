9fbd0150ade8c0edad58f9ec9577d69724977b06
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-12 21:42:34 +0100

    Add more data from seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 file = File.read(Rails.root.join 'books.json')
 json = JSON.parse(file).flatten
 
+Author.create(name: "Martin Fowler")
+Author.create(name: "Russ Olsen")
+Author.create(name: "Rober C. Martin")
+
 json.each do |book|
-  Book.create(title: book['title'],
+  b = Book.create(title: book['title'],
               subtitle: book['subtitle'],
               isbn: book['isbn10'],
               isbn_13: book['isbn13'],
@@ -10,10 +14,28 @@ json.each do |book|
               pages: book['pageCount'],
               description: book['description'],
               image_link: book['thumbnail'],
-              language: book['language']
+              language: book['language'],
              )
+  b.authors << Author.first
+  b.save
 end
 
-Author.create(name: "Test One")
-Author.create(name: "Two Test")
-Author.create(name: "Mr Test Three")
+['Dennis', 'Paul', 'Martin'].each do |name|
+  User.create email:  "#{name}@test.com",
+              password: 'secredfdaff',
+              first_name: name,
+              last_name: 'Lastname'
+end
+
+User.first(2).each do |user|
+  user.books << Book.all
+  user.save
+end
+
+(1..100).each do |number|
+  User.create email:  "Dummy_user#{number}@test.com",
+              password: 'secredfdaff',
+              first_name: "Dummy_#{number}",
+              last_name: 'Dummkopf'
+end
+
