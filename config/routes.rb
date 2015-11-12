724918158abd18f797b134bc632028e7dea77c6b
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-12 14:22:30 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :show]
+  get 'users', to: 'users#index'
   post '/books', to: 'books#search', as: 'book_search'
+
+  resources :books, only: [:index, :show] do
+    resources :users, only: [:index, :show]
+  end
+
+  resources :user do
+    resources :books, only: [:index, :show]
+    resources :user, only: [:index, :show]
+  end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root to: "home#index"
