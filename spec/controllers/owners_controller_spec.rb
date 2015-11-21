5347784fe66766cd30ba73f93ecd47c8d518a716
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-21 20:21:12 +0100

    Add new controller spec for owners of book

---
+++ spec/controllers/owners_controller_spec.rb
@@ -0,0 +1,15 @@
+describe OwnersController, type: :controller do
+  let(:user_with_books) { create :user, :with_books }
+
+  describe 'GET /books/:book_id/owners' do
+    it 'shows all owners of the book' do
+      book = user_with_books.books.first
+      user_2 = create :user
+      book.owners << user_2
+      get :index, book_id: user_with_books.books.first.id
+      expect(assigns(:users).count).to eq 2
+      expect(assigns(:users).first).to eq user_with_books
+      expect(assigns(:users).second).to eq user_2
+    end
+  end
+end
