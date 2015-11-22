a9b970852bf7553cca80fdc8e9e0d3330190c639
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-22 01:29:21 +0100

    Add BorrowRequest index

--- app/controllers/borrow_requests_controller.rb
+++ app/controllers/borrow_requests_controller.rb
@@ -1,4 +1,8 @@
 class BorrowRequestsController < ApplicationController
+  def index
+    @borrow_requests = BorrowRequest.where(requester_id: params[:user_id])
+  end
+
   def create
     owner = User.find(params[:user_id])
     requester = User.find(params[:requester_id])
--- spec/controllers/borrow_requests_controller_spec.rb
+++ spec/controllers/borrow_requests_controller_spec.rb
@@ -4,14 +4,26 @@ describe BorrowRequestsController, type: :controller do
   let(:borrower) { create :user }
   let(:owner) { create :user, :with_books }
   let(:book_to_borrow) { owner.books.first }
-
-  describe 'GET index' do
-    skip
+  let(:borrow_request) do
+    BorrowRequest.create(requester: borrower,
+                         owner: owner,
+                         book: book_to_borrow)
   end
 
-  describe 'POST create' do
+  describe 'GET /users/:user_id/books/:id' do
+    it 'returns all requests of the user' do
+      borrow_request
+      get :index, user_id: owner
+      expect(assigns(:borrow_requests).count).to eq 0
+      get :index, user_id: borrower
+      expect(assigns(:borrow_requests).count).to eq 1
+    end
+  end
+
+  describe 'POST /users/:user_id/books/:id' do
     it 'creates a new borrow request' do
-      post :create, user_id: owner, requester_id: borrower, book_id: book_to_borrow
+      post :create, user_id: owner,
+        requester_id: borrower, book_id: book_to_borrow
       expect(owner.reload.lent_requests.first).to be_truthy
       expect(owner.reload.lent_requests.first).to eq BorrowRequest.first
       expect(borrower.reload.borrow_requests.first).to eq BorrowRequest.first
