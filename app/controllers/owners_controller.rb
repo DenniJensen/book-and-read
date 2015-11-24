1b487a40a09c82c80f105f140c8cd47951f77719
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-24 21:28:08 +0100

    Fix showing book owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,14 +4,14 @@ class OwnersController < ApplicationController
   add_breadcrumb "Buchbesitzer", :book_owners_path
 
   def index
-    @book = Book.find_by(params[:books_id])
+    @book = Book.find(params[:book_id])
     @users = @book.owners if @book
     render 'users/index'
   end
 
   def show
     add_breadcrumb "Buchbesitzer", :book_owner_path
-    @book = Book.find_by(params[:books_id])
+    @book = Book.find(params[:book_id])
     @users = @book.owners if @book
   end
 end
