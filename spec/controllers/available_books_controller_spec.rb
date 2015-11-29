72068e0d1960c0a6fa52db8194879a1464365389
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-29 15:05:56 +0100

    Add skipping the authentication in specs

--- spec/controllers/available_books_controller_spec.rb
+++ spec/controllers/available_books_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe AvailableBooksController do
+  before do
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+    allow(controller).to receive(:current_user).and_return(user)
+  end
+
   describe 'GET /books' do
     it 'show all books with in ownership' do
       books_owner = create :user, :with_books
